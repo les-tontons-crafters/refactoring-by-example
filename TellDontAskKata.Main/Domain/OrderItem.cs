7210ff45d806f400b497b4754e7638f43646501c
Yoan Thirion <yoan.thirion@gmail.com>
2023-03-10 09:08:13 +0400

    Move factory logic to OrderItem constructor

--- TellDontAskKata.Main/Domain/OrderItem.cs
+++ TellDontAskKata.Main/Domain/OrderItem.cs
@@ -5,24 +5,26 @@ namespace TellDontAskKata.Main.Domain
 {
     public class OrderItem
     {
-        public Product Product { get; private init; }
-        public int Quantity { get; private init; }
-        public decimal TaxedAmount { get; private init; }
-        public decimal Tax { get; private init; }
+        public Product Product { get; }
+        public int Quantity { get; }
+        public decimal TaxedAmount { get; }
+        public decimal Tax { get; }
 
-        public static OrderItem New(CreateOrderItem itemRequest, Product product)
+        private OrderItem(Product product, int quantity)
         {
+            Product = product;
+            Quantity = quantity;
+
             var unitaryTax = Round((product.Price / 100m) * product.Category.TaxPercentage);
             var unitaryTaxedAmount = Round(product.Price + unitaryTax);
 
-            return new OrderItem
-            {
-                Product = product,
-                Quantity = itemRequest.Quantity,
-                Tax = Round(unitaryTax * itemRequest.Quantity),
-                TaxedAmount = Round(unitaryTaxedAmount * itemRequest.Quantity)
-            };
+            Tax = Round(unitaryTax * quantity);
+            TaxedAmount = Round(unitaryTaxedAmount * quantity);
         }
+
+
+        public static OrderItem New(CreateOrderItem itemRequest, Product product)
+            => new(product, itemRequest.Quantity);
 
         private static decimal Round(decimal amount) => decimal.Round(amount, 2, MidpointRounding.ToPositiveInfinity);
     }
